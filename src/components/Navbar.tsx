1210555b5b40be5f93e1a10fbdac57f1c2df4590
egileekada <egileoniso.ekada@gmail.com>
2022-08-08 07:18:52 +0100

    update onNavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar() {
         <div className=' bg-[#100030] flex justify-between items-center w-full h-24 px-28  ' >
             <p className=' text-2xl text-white font-NotoSans-ExtraBold ' >Hero</p>
             <div className=' flex items-center text-[#e9f7d7] font-NotoSans-Medium ' >
-                <a className='  mx-6 ' >Explore</a>
-                <a className='  mx-6 ' >Create</a>
-                <a className='  mx-6 ' >Stats</a>
+                <a href='/explore'  className=' mx-6 ' >Explore</a>
+                <a className=' mx-6 ' >Create</a>
+                <a className=' mx-6 ' >Stats</a>
                 <img src={search} className=" w-7 mx-6 " />
                 <img src={shop} className=" w-7 mx-6 " />
                 <button className='  bg-gradient-to-r from-[#A100E2] via-[#7257CE] to-[#4E5FC0] w-40 ml-10 mx-3  h-11 rounded-md ' >
