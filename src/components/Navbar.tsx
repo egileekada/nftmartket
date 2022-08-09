05b5bd3062d908a13483cf1bf535aba2cde5dc0a
egileekada <egileoniso.ekada@gmail.com>
2022-08-09 13:52:14 +0100

    update on collections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import search from "../assets/images/search.svg"
 import shop from "../assets/images/shop.svg"
+import logo from "../assets/images/Logo.png"
 
-export default function Navbar() {
+export default function Navbar(props: any) {
     return (
-        <div className=' xl:w-1360px flex justify-between items-center w-full h-24 px-28  ' >
-            <a href='/' className=' text-2xl text-white font-NotoSans-ExtraBold ' >Hero</a>
+        <div className=' xl:w-1360px flex justify-between items-center w-full h-24 px-10  ' >
+            <a href='/' className=' text-2xl text-white font-NotoSans-ExtraBold ' ><img src={logo} alt="logo" className="w-11 h-11" /></a>
             <div className=' flex items-center text-[#fff] font-NotoSans-Medium ' >
                 <a href='/explore'  className=' mx-6 ' >Explore</a>
                 <a className=' mx-6 ' >Create</a>
